bda9fcab7aeb260c498a8440806e7a749e80720e
DanilUltrakill <yaremakadanil@gmail.com>
2019-06-26 17:52:45 +0700

    store+

--- Game/Game/Gaming.cs
+++ Game/Game/Gaming.cs
@@ -6,17 +6,48 @@ using System.Threading.Tasks;
 
 namespace Game
 {
-    class Gaming
+    public class Gaming
     {
-        public int hp = 100, hpenemy = 50, gold = 0;
-        public int weapon = 1, armor = 10;
+
+        public int hp = 100, hpenemy = 50, gold = 0, heal = 0;
+        public int weapon = 1, armor = 5;
         public bool canfight = true;
         static Random rand = new Random();
 
-       //public void hit(int weapon,int armor)
-       // {
-       //     hpenemy = hpenemy - (1+5 * weapon);
-       //     hp = hp - 10 + armor;
-       // }
+        public void rewarding(int reward)
+        {
+            gold = gold + reward;
+        }
+
+        public void output()
+        {
+
+        }
+
+        public void spendinggold(int cost)
+        {
+            gold = gold + cost;
+        }
+
+        public void buyingheal(int healp)
+        {
+            heal = heal + healp;
+        }
+
+        public void buyingarmor(int armorp)
+        {
+            armor = armorp;
+        }
+
+        public void buyingweapon(int weaponp)
+        {
+            weapon = weaponp;
+        }
+
+        //public void hit(int weapon,int armor)
+        // {
+        //     hpenemy = hpenemy - (1+5 * weapon);
+        //     hp = hp - 10 + armor;
+        // }
     }
 }
